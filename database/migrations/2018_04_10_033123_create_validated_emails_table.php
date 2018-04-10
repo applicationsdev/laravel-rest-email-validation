e48e7c7a7d9b6b702630a9ba5aac69dc58dc0e8f
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-04-10 04:34:11 +0000

    ValidatedEmail migration

---
+++ database/migrations/2018_04_10_033123_create_validated_emails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateValidatedEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('validated_emails', function (Blueprint $table) {
+            $table->increments('id');
+            // By IETF, the max allowed limit of email length is {64}@{255}
+            // but a custom-defined limit can increase our app's security & performance
+            $table->string('email', 100)->unique();
+            $table->timestampTz('validated_timestamp')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('validated_emails');
+    }
+}
