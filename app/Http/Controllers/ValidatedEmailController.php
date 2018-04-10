50f7b34bade14e50f49672567017010054c671dc
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-04-10 20:11:20 +0000

    Email validation controller

---
+++ app/Http/Controllers/ValidatedEmailController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ValidatedEmail;
+
+class ValidatedEmailController extends Controller
+{
+    // Since value setters are used below, setting value here can be considered redundant
+    // but if that property becomes critical, this can be a good security practice
+    // for future updates of this unit
+    private $validationState = false;
+    
+    private function setValidationStateTrue() {
+        return $this->validationState = true;
+    }
+    
+    private function setValidationStateFalse() {
+        return $this->validationState = false;
+    }
+    
+    public function getValidationState() {
+        return $this->validationState;
+    }
+    
+    /**
+    * Display the specified resource.
+    *
+    * @param  str  $email
+    * @return \Illuminate\Http\Response
+    */
+    public function show($email)
+    {
+        
+        $this->setValidationStateFalse();
+        
+        // Check if Email exists
+        if (!$validatedEmail = ValidatedEmail::where('email', '=', $email)->first()) {
+            
+            return response('Email was not found', 404);
+            
+        } else {
+            
+            // Check if Email has been already validated during current date
+            $lastValidationTimestampArray = explode(' ', $validatedEmail->validated_timestamp);
+            
+            if ($lastValidationTimestampArray[0] == date('Y-m-d')) {
+               
+               $this->setValidationStateTrue();
+               
+               return response()->json([
+                    'email' => $validatedEmail->email,
+                    'validated_at' => $validatedEmail->validated_timestamp,
+                    'is_valid' => $this->getValidationState(),
+                ], 200);
+            
+            // Else validate Email now
+            } else {
+                
+                $this->setValidationStateFalse();
+                
+                if (!filter_var($validatedEmail->email, FILTER_VALIDATE_EMAIL)) {
+                
+                    return response()->json([
+                        'email' => $validatedEmail->email,
+                        'is_valid' => $this->getValidationState(),
+                    ]); // when resource is found & HTTP status code is undefined,
+                        // then we receive code 200 anyway
+                
+                } else {
+            
+                    $validatedEmail->validated_timestamp = date("Y-m-d H:i:s");
+                    $validatedEmail->save();
+                    
+                    $this->setValidationStateTrue();
+            
+                    return response()->json([
+                        'email' => $validatedEmail->email,
+                        'validated_at' => $validatedEmail->validated_timestamp,
+                        'is_valid' => $this->getValidationState(),
+                    ], 200);
+                }
+            }
+        }
+    }
+}
