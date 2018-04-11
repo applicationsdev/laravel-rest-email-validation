ae12b76bc2488ea4bc9fbac42028b5cbde67e775
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-04-11 17:48:47 +0000

    PHPunit EmailValidationApiTest upgrade

--- tests/Feature/EmailValidationApiTest.php
+++ tests/Feature/EmailValidationApiTest.php
@@ -1,54 +1,108 @@
 <?php
 
+/**
+ * RESTful API TESTING example for Development Environment
+ * by @applicationsdev
+ * https://github.com/applicationsdev
+ */
+
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class EmailValidationApiTest extends TestCase
 {
-    // Data for testing
-    protected $emailExample = [
-        'doesNotExist' => 'testrandomtest@randomtest.com',
-        'existsButNotValid' => 'DLRHuUS4sDpeXn1uAE',
-        'existsAndValid' => 'mackenzie.block@abshire.com',
-    ];
+    /**
+     * # DATA GENERATORS definition
+     */
     
+    // Creates one unique email & stores it in DB
+    // note: unless we pass TRUE as argument, any other value will generate invalid email
+    protected function createOneUniqueEmailRecord($validEmail) {
+        
+        // Ensures that one unique email is generated
+        do {
+            if ($validEmail === true) {
+                $oneUniqueEmail = $this->generateValidEmail();
+                
+            } else {
+                $oneUniqueEmail = $this->generateInvalidEmail();
+            }
+            
+        } while ($record = DB::table('validated_emails')
+            ->where('email', '=', $oneUniqueEmail)->first());
+            
+        // Stores generated email into DB, so that it can be accessed later via REST API
+        DB::table('validated_emails')->insert([
+            'email' => $oneUniqueEmail,
+        ]);
+        
+        return $oneUniqueEmail;
+    }
+    
+    // Simple & useful data generation helpers
+    public function generateValidEmail() {
+        return str_random(16) . '@' . str_random(10) . '.com';
+    }
+
+    public function generateInvalidEmail() {
+        return '@invalid-structure-or-script-injection-attempt.' . str_random(8);
+    }
+    
+    public function generateNonExistantEmail() {
+        return str_random(50) . '@' . str_random(15) . '.com';
+        // making this function more secure is unnecessary complexity for current version
+    }
+    
+    
+    /**
+     * # API TESTS definition
+     */
+    
+    // Simulate & test API's behavior to non-existant email
     public function testDoesNotExist()
     {
         $response = $this
             ->withHeaders(['Accept' => 'application/json'])
-            ->json('GET', 'api/validate/email/' . $this->emailExample['doesNotExist']);
+            ->json('GET', 'api/validate/email/' . $this->generateNonExistantEmail());
         
         $response->assertStatus(404);
     }
     
+    // Simulate & test API's behavior to existant invalid email
     public function testExistsButNotValid()
     {
+        $generatedData = $this->createOneUniqueEmailRecord(false);
+        
         $response = $this
             ->withHeaders(['Accept' => 'application/json'])
-            ->json('GET', 'api/validate/email/' . $this->emailExample['existsButNotValid']);
+            ->json('GET', 'api/validate/email/' . $generatedData);
         
         $response
             ->assertStatus(200)
             ->assertHeader('content-type', 'application/json')
             ->assertJsonFragment([
-                'email' => $this->emailExample['existsButNotValid'],
+                'email' => $generatedData,
                 'is_valid' => false,
             ]);
     }
     
+    // Simulate & test API's behavior to existant valid email
     public function testExistsAndValid()
     {
+        $generatedData = $this->createOneUniqueEmailRecord(true);
+        
         $response = $this
             ->withHeaders(['Accept' => 'application/json'])
-            ->json('GET', 'api/validate/email/' . $this->emailExample['existsAndValid']);
+            ->json('GET', 'api/validate/email/' . $generatedData);
         
         $response
             ->assertStatus(200)
             ->assertHeader('content-type', 'application/json')
             ->assertJsonFragment([
-                'email' => $this->emailExample['existsAndValid'],
+                'email' => $generatedData,
                 'is_valid' => true,
             ]);
     }
